ca6ccfc662bddfa94cc15179921e58152feb28e0
Tom Gagliano <tgaglianoll@gmail.com>
2024-09-19 09:51:02 -0400

    add new login file and start initial testing of routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Footer } from "./Footer";
 import { Signup } from "./Signup";
+import { Login } from "./Login";
 
 function App() {
   return (
     <div>
       <Signup />
+      <Login />
       <Footer />
     </div>
   );
---
+++ src/Login.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
+
+export function Login() {
+  const [errors, setErrors] = useState([]);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [unsuccessful, setUnsuccessful] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    setSuccessMessage("");
+    setUnsuccessful("");
+
+    const params = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+
+    axios
+      .post("http://localhost:3000/api/users/login", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+        setSuccessMessage("You have successfully logged in!");
+
+        // Store token or session details if using token-based authentication
+        localStorage.setItem("token", response.data.token);
+      })
+      .catch((error) => {
+        console.log(error.response?.data?.errors || error.message);
+        setErrors(error.response?.data?.errors || []);
+        setUnsuccessful(
+          "The credentials you entered do not match our records."
+        );
+      });
+  };
+
+  return (
+    <div id="login">
+      <br />
+      <h1 className="login-title">Login</h1>
+
+      {successMessage && <p className="success-message">{successMessage}</p>}
+      {unsuccessful && <p className="unsuccessful-message">{unsuccessful}</p>}
+
+      <ul>
+        {errors.map((error, index) => (
+          <li key={index} className="error-message">
+            {error}
+          </li>
+        ))}
+      </ul>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input name="email" type="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input name="password" type="password" />
+        </div>
+        <button className="submit">Login</button>
+      </form>
+    </div>
+  );
+}
