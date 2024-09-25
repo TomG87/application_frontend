1006cbd949762a9c1a3da2f9e0e593f6c73ca0fc
Tom Gagliano <tgaglianoll@gmail.com>
2024-09-25 16:28:36 -0400

    install react-router-dom and implement routing for Signup and Login components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Footer } from "./Footer";
 import { Signup } from "./Signup";
 import { Login } from "./Login";
 
 function App() {
   return (
-    <div>
-      <Signup />
-      <Login />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/api/users/register" element={<Signup />} />
+          <Route path="/api/users/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
