dd929fd0a7cba38fc6aa97e8468dd2b64463de21
Tom Gagliano <tgaglianoll@gmail.com>
2024-12-06 17:20:24 -0500

    adjust CSS to move signup icon below login form for improved UI

--- src/Login.jsx
+++ src/Login.jsx
@@ -52,11 +52,6 @@ export function Login() {
       <br></br>
       <br></br>
       <h1 className="login-title">Login</h1>
-      <div className="register-link">
-        <Link to="/register" className="action-link">
-          <PiUserCircleDuotone className="action-icon" /> Signup
-        </Link>
-      </div>
 
       {successMessage && <p className="success-message">{successMessage}</p>}
       {unsuccessful && <p className="unsuccessful-message">{unsuccessful}</p>}
@@ -85,6 +80,11 @@ export function Login() {
       <br></br>
       <br></br>
       <br></br>
+      <div className="register-link">
+        <Link to="/register">
+          <PiUserCircleDuotone className="action-icon" /> Signup
+        </Link>
+      </div>
     </div>
   );
 }
