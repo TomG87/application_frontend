3e74b8d42a14f566b6895a1350c081e341b908d8
Tom Gagliano <tgaglianoll@gmail.com>
2025-01-01 19:33:53 -0500

    add image.png file for app background

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { ApplicationPost } from "./ApplicationPost";
 import ApplicationIndex from "./ApplicationIndex";
 import ApplicationEdit from "./ApplicationEdit";
 import React, { useEffect, useState } from "react";
+import image from "./images/image.png";
 import logo from "./images/application-trackstar-high-resolution-logo.png";
 
 function App() {
@@ -48,31 +49,34 @@ function App() {
   return (
     <Router>
       <div>
-        <div className="app-logo">
-          <img src={logo} alt="Application TrackStar Logo" className="logo" />
-          <Routes>
-            <Route path="/register" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/applications"
-              element={
-                <ApplicationIndex
-                  applications={applications}
-                  userId={userId}
-                  token={token}
-                />
-              }
-            />
-            <Route
-              path="/applications/create"
-              element={<ApplicationPost userId={userId} token={token} />}
-            />
-            <Route
-              path="/applications/edit/:id"
-              element={<ApplicationEdit userId={userId} token={token} />}
-            />
-          </Routes>
-          <Footer />
+        <div className="app-background">
+          <img src={image} alt="Background Design" />
+          <div className="app-logo">
+            <img src={logo} alt="Application TrackStar Logo" className="logo" />
+            <Routes>
+              <Route path="/register" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/applications"
+                element={
+                  <ApplicationIndex
+                    applications={applications}
+                    userId={userId}
+                    token={token}
+                  />
+                }
+              />
+              <Route
+                path="/applications/create"
+                element={<ApplicationPost userId={userId} token={token} />}
+              />
+              <Route
+                path="/applications/edit/:id"
+                element={<ApplicationEdit userId={userId} token={token} />}
+              />
+            </Routes>
+            <Footer />
+          </div>
         </div>
       </div>
     </Router>
