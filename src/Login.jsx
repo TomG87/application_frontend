84cd6a6e7c4e9559a1875d8c9357f504ab8fa8f3
Tom Gagliano <tgaglianoll@gmail.com>
2025-01-06 20:44:06 -0500

    add extra space for email and password fields for better clarity on Login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -72,11 +72,11 @@ export function Login() {
 
       <form className="login-form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">Email: </label>
           <input name="email" type="email" />
         </div>
         <div>
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password">Password: </label>
           <input name="password" type="password" />
         </div>
         <button className="submit">Login</button>
